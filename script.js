914284bb4697bf2a4885e272ab6de642c142e1de
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 12:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let arr = [1, 2, 3, 4];
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(arr);
+    }, 3000);
+}).then((arr) => {
+    let newArr = arr.filter((el) => el % 2 !== 0);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            document.getElementById('output').textContent = newArr;
+            resolve(arr);
+        }, 1000);
+    });
+}).then((arr) => {
+    let newArr = arr.map((el) => (el % 2 === 0 ? el * 2 : el));
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            document.getElementById('output').textContent = newArr;
+            resolve(arr);
+        }, 2000);
+    });
+});
+
+
